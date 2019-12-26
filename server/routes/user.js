6ecc26ae9fb050d6b7db2e485d2f7fcb7ed95de3
jsanchezpc <jsanchezpc@hotmail.com>
2019-12-26 02:28:34 +0100

    password check and readme update

--- server/routes/user.js
+++ server/routes/user.js
@@ -10,33 +10,43 @@ app.post('/signup', (req, res) => {
      console.log('New signup request, creating user..........');
      let body = req.body;
 
-     let newUser = new User({
-          username: body.username,
-          email: body.email,
-          password: bcrypt.hashSync(body.password, 10)
-     });
-
-     newUser.save((err, userDB) => {
-
-          if (err) {
-               return res.status(400).json({
-                    ok: false,
-                    err
-               });
-          }
-
-          let token = jwt.sign({
-               user: userDB
-          }, process.env.SEED, { expiresIn: process.env.CAD_TOKEN });
-
-          res.json({
-               ok: true,
-               usuario: userDB,
-               token: token
+     // password between 7 to 16 characters which contain only characters, numeric digits, 
+     // underscore and first character must be a letter
+     const check = /^[A-Za-z]\w{6,24}$/;
+     if (body.password !== '' || body.password.match(check)) {
+          let newUser = new User({
+               username: body.username,
+               email: body.email,
+               password: bcrypt.hashSync(body.password, 10)
           });
 
+          newUser.save((err, userDB) => {
 
-     });
+               if (err) {
+                    return res.status(400).json({
+                         ok: false,
+                         err
+                    });
+               }
+
+               let token = jwt.sign({
+                    user: userDB
+               }, process.env.SEED, { expiresIn: process.env.CAD_TOKEN });
+
+               res.json({
+                    ok: true,
+                    usuario: userDB,
+                    token: token
+               });
+
+
+          });
+     } else {
+          res.json({
+               ok: false,
+               err: 'Wrong password!'
+          });
+     }
 });
 
 app.post('/login', (req, res) => {
